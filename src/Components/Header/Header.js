e61b3fc52bc32b56d2c10def5719398c10aca67f
azaadd <abulkalam.kta@gmail.com>
2023-06-14 16:57:26 +0600

    added header info section in home

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -4,17 +4,20 @@ import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
-        <nav>
-            <h2>WebExpert Farm</h2>
-            <div>
-                <Link to="/">Home</Link>
-                <Link to="/statistics">Statistics</Link>
-                <Link to="/appliedjobs">Applied Jobs</Link>
-                <Link to="/blog">Blog</Link>
+        <section className='header'>
+            <nav>
+                <h2>WebExpert Farm</h2>
+                <div>
+                    <Link to="/">Home</Link>
+                    <Link to="/statistics">Statistics</Link>
+                    <Link to="/appliedjobs">Applied Jobs</Link>
+                    <Link to="/blog">Blog</Link>
 
-            </div>
-            <button>Start Applying</button>
-        </nav>
+                </div>
+                <button className='apply'>Start Applying</button>
+            </nav>
+            
+        </section>
     );
 };
 
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,10 +1,18 @@
 import React from 'react';
-import './Home.css'
+import './Home.css';
+import personImage from '../../images/developer.jpg';
 
 const Home = () => {
     return (
-        <div>
-            
+        <div className='home'>
+            <div className='header-info-wrap'>
+                <div className='header-info'>
+                    <h1>Code a New <span className='d-color'>Career</span></h1>
+                    <p>Equip yourself with the data and knowledge needed to program a better future.</p>
+                    <button className='apply'>Get Started</button>
+                </div>
+                <img className='person-img' src={personImage} alt="" />
+            </div>
         </div>
     );
 };
